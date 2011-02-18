6b46a3d7c3f15bffedcaa8278dad3c5859aab147
Francis Giraldeau <francis.giraldeau@usherbrooke.ca>
2011-02-18 16:48:01 -0500

    Add UST trace points

--- src/bossthread.c
+++ src/bossthread.c
@@ -8,7 +8,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#ifdef WITH_UST
+#include <ust/marker.h>
+#endif
 #include "calibrate.h"
+
+#ifndef WITH_UST
+#define trace_mark(...)
+#endif
 
 #define MAX_THREAD 4
 
@@ -20,14 +27,20 @@ struct worker_data {
 void *worker(void *data) {
 	struct worker_data *info = (struct worker_data *) data;
 	int i;
+	pthread_t self = pthread_self();
+	trace_mark(ust, worker_entry, "id %lu", self);
+
 	for(i=0; i<info->count; i++) {
 		info->res++;
 	}
+
+	trace_mark(ust, worker_exit, "id %lu", self);
 	return 0;
 }
 
 int main(int argc, char **argv) {
 
+	trace_mark(ust, main_entry, "%d", 9);
 	pthread_t threads[MAX_THREAD];
 	void *status;
 	int i = 0;
