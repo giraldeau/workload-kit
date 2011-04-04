abd01f1ca1b098e37751d38131aed895356f7bcf
Francis Giraldeau <francis.giraldeau@usherbrooke.ca>
2011-04-04 14:20:50 -0400

    Adding new utility "inception"
    The utility inception spawn children recursively. Each process sleeps and wait
    for it's child to terminate. Each children spawned sleep twice the time of it's
    parent.

---
+++ src/inception.c
@@ -0,0 +1,85 @@
+/*
+ * inception.c
+ *
+ * This program creates recursively children process that sleeps for a while
+ *
+ *  Created on: 2011-02-18
+ *      Author: francis
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include <error.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int mili = 0;
+int children = 0;
+
+void usage(char *prog) {
+	printf("Usage: %s TIME CHILDREN\n\n", prog);
+	printf("    TIME     : sleep time in miliseconds\n", prog);
+	printf("    CHILDREN : number of children to spawn\n", prog);
+}
+
+int do_run() {
+	/* self sleep first */
+	if (do_sleep() < 0) {
+		error(0, errno, "error in do_sleep");
+		return -1;
+	}
+
+	/* then spawn children */
+	if (spawn_child() < 0) {
+		error(0, errno, "error in span_children");
+		return -1;
+	}
+}
+
+int do_sleep() {
+
+	struct timespec t;
+	t.tv_sec = mili / 1000;
+	t.tv_nsec = (mili % 1000) * 1000000;
+
+	if (nanosleep(&t, NULL) < 0) {
+		return -1;
+	}
+}
+
+int spawn_child() {
+	int child;
+	int stat_loc;
+
+	children--;
+	mili = mili * 2;
+
+	if (children == 0)
+		return 0;
+
+	if((child = fork()) < 0) {
+		return -1;
+	} else if (child == 0) {
+		do_run();
+	} else {
+		waitpid(child, &stat_loc, 0);
+	}
+
+	return 0;
+}
+
+int main (int argc, char **argv) {
+
+	if (argc < 3) {
+		usage(argv[0]);
+		return 1;
+	}
+
+	mili = atoi(argv[1]);
+	children = atoi(argv[2]);
+
+	do_run();
+
+	return 0;
+}
