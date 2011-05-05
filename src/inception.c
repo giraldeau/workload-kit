e99be3230849d595c3fa02249dd7c8e9c3cc5f1f
Francis Giraldeau <francis.giraldeau@gmail.com>
2011-05-05 17:43:02 -0400

    Fix type warning

--- src/inception.c
+++ src/inception.c
@@ -13,6 +13,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 
 int mili = 0;
 int children = 0;
@@ -40,12 +41,17 @@ int do_run() {
 int do_sleep() {
 
 	struct timespec t;
+	struct timeval t1, t2;
 	t.tv_sec = mili / 1000;
 	t.tv_nsec = (mili % 1000) * 1000000;
 
+    printf("t.tv_sec=%lu t.tv_nsec=%lu\n", t.tv_sec, t.tv_nsec);
+    gettimeofday(&t1, NULL);
 	if (nanosleep(&t, NULL) < 0) {
 		return -1;
 	}
+    gettimeofday(&t2, NULL);
+    printf("delay=%lu\n", t2.tv_usec - t1.tv_usec);
 }
 
 int spawn_child() {
