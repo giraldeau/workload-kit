758298de642c6da4448e8776c12e2bc1a0e4855c
Francis Giraldeau <francis.giraldeau@gmail.com>
2011-05-05 17:43:38 -0400

    RPC client server to test dependency analysis

---
+++ src/srvhog.c
@@ -0,0 +1,100 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "calibrate.h"
+
+#define MSLEEP 100
+#define UHOG 100000
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int do_sleep(int mili) {
+
+	struct timespec t;
+	struct timeval t1, t2;
+	t.tv_sec = mili / 1000;
+	t.tv_nsec = (mili % 1000) * 1000000;
+
+    printf("t.tv_sec=%lu t.tv_nsec=%lu\n", t.tv_sec, t.tv_nsec);
+    gettimeofday(&t1, NULL);
+	if (nanosleep(&t, NULL) < 0) {
+		return -1;
+	}
+    gettimeofday(&t2, NULL);
+    printf("delay=%lu\n", t2.tv_usec - t1.tv_usec);
+}
+
+int do_hog(long x) {
+	volatile y = 0, i = 0;
+	for(i=0;i<x;i++) {
+		y++;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno;
+     int yes = 1;
+     socklen_t clilen;
+     char buffer[256];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+
+     printf("server starting\n");
+	 // 100ms by default
+	 unsigned long count = calibrate(UHOG);
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *) &yes, sizeof(yes)) < 0)
+    	 error("ERROR while setting socket option");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     printf("server waiting cmd\n");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,256);
+     n = read(newsockfd,buffer,255);
+     if (n < 0) error("ERROR reading from socket");
+     printf("server recv cmd: %s (%d)\n",buffer,n);
+     if (strncmp(buffer, "sleep", n) == 0) {
+    	 printf("do_sleep\n");
+    	 do_sleep(MSLEEP);
+     } else if (strncmp(buffer, "hog", n) == 0) {
+    	 printf("do_hog\n");
+    	 do_hog(count);
+     } else {
+    	 printf("unknown command\n");
+     }
+     n = write(newsockfd,"done",18);
+     if (n < 0) error("ERROR writing to socket");
+     close(newsockfd);
+     close(sockfd);
+     return 0; 
+}
