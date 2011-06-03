cadad9405b2251e0004d1246c9923d04108a615a
Francis Giraldeau <francis.giraldeau@usherbrooke.ca>
2011-06-03 17:33:27 -0400

    Testing

--- src/cyg.c
+++ src/cyg.c
@@ -1,23 +1,18 @@
-#include <stdio.h>
 #include <execinfo.h>
-//#include <ust/marker.h>
-
-// will add a cache for the function names later. 
+#include <ust/marker.h>
 
 void __cyg_profile_func_enter (void *this_fn,
                                          void *call_site)
 {
 	char *funcname = backtrace_symbols(&this_fn, 1)[0];
-	//ust_marker(entry, "func_entry %s", funcname);
-	printf("entry %p\n", this_fn);
+	ust_marker(entry, "func_entry %s", funcname);
 }
 
 void __cyg_profile_func_exit  (void *this_fn,
                                          void *call_site)
 {
 	char *funcname = backtrace_symbols(&this_fn, 1)[0];
-	//ust_marker(exit, "func_exit %s", funcname);
-	printf("exit %p\n", this_fn);
+	ust_marker(exit, "func_exit %s", funcname);
 }
 
 
