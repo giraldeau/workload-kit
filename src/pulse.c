850a13433bb4426b7075d263a6d8a974a7a65d20
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-02-13 10:14:37 -0500

    Integrate spin and pincpu into pulse
    Trigger pulses on defined CPU set. Threads are dispatched in a round-robin way.
    
    Example:
    
      wk-pulse --cpuset 0,0,1 --thread 4
    
      thread 0 -> CPU 0
      thread 1 -> CPU 0
      thread 2 -> CPU 1
      thread 3 -> CPU 0
    
    Thus, thread 0,1,3 will wake-up at the same time and fight for the CPU for the
    specified period of time. The period is fixed, if contention is very high, then
    there is no guarantee a thread will run at all.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- src/pulse.c
+++ src/pulse.c
@@ -13,13 +13,19 @@
  * arguments: thread, delay
  */
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <inttypes.h>
 #include <pthread.h>
 #include <getopt.h>
 #include <sys/time.h>
+#include <unistd.h>
+#include <sched.h>
+
 #include "spin.h"
+#include "pincpu.h"
 
 #define DEFAULT_SPIN 100000 // 100ms
 #define DEFAULT_DURATION 1000000 // 1s
@@ -35,6 +41,8 @@ struct experiment {
     int verbose;
     int freq;
     long duration;
+    int pin;
+    struct pincpu cpuset;
 };
 
 __attribute__((noreturn))
@@ -44,13 +52,13 @@ static void usage(void) {
     fprintf(stderr, "--spin S         spin duration in microseconds (default = 100 000 us)\n");
     fprintf(stderr, "--thread N       number of threads to be spawned (default = 2)\n");
     fprintf(stderr, "--frequency      frequency in HZ of the CPU bursts per thread (default = 4)\n");
+    fprintf(stderr, "--cpuset         pin threads to these CPUs (round-robin, comma separated cpu id)\n");
     fprintf(stderr, "--duration       duration in microseconds of the experiment (default = 1 000 000 us\n");
     fprintf(stderr, "--verbose        be verbose\n");
     fprintf(stderr, "--help           print this message and exit\n");
     fprintf(stderr, "\n");
     exit(EXIT_FAILURE);
 }
-
 
 static void parse_opts(int argc, char **argv, struct experiment *exp) {
     int opt;
@@ -62,6 +70,7 @@ static void parse_opts(int argc, char **argv, struct experiment *exp) {
             { "thread",     1, 0, 't' },
             { "freq",       1, 0, 'f' },
             { "spin",       1, 0, 's' },
+            { "cpuset",     1, 0, 'c' },
             { "verbose",    0, 0, 'v' },
             { 0, 0, 0, 0 }
     };
@@ -70,8 +79,9 @@ static void parse_opts(int argc, char **argv, struct experiment *exp) {
     exp->spin = DEFAULT_SPIN;
     exp->freq = DEFAULT_FREQ;
     exp->duration = DEFAULT_DURATION;
+    exp->pin = 0;
 
-    while ((opt = getopt_long(argc, argv, "hd:t:f:s:v", options, &idx)) != -1) {
+    while ((opt = getopt_long(argc, argv, "hd:t:f:s:c:v", options, &idx)) != -1) {
         switch (opt) {
         case 'd':
             exp->duration = atol(optarg);
@@ -84,6 +94,14 @@ static void parse_opts(int argc, char **argv, struct experiment *exp) {
 			break;
 		case 's':
 			exp->spin = atoi(optarg);
+			break;
+		case 'c':
+			exp->pin = 1;
+			if (pincpu_parse(&exp->cpuset, optarg) < 0) {
+				printf("Error parsing cpuset\n");
+				usage();
+			}
+			dump_cpuset(&exp->cpuset);
 			break;
         case 'h':
             usage();
@@ -98,6 +116,21 @@ static void parse_opts(int argc, char **argv, struct experiment *exp) {
     	printf("Warning: Spin duration is too long for requested frequency."
     			"CPU may spin all the time.\n");
     }
+    // check cpuset
+    if (!exp->cpuset.nrcpus) {
+    	printf("Error: cpuset empty\n");
+    	dump_cpuset(&exp->cpuset);
+    	usage();
+    }
+}
+
+void pulse_pincpu(struct spin_args *arg) {
+	struct experiment *exp = arg->spin->data;
+	int cpu = exp->cpuset.cpus[arg->id % exp->cpuset.nrcpus];
+	printf("pinning thread %d to cpu %d\n", arg->id, cpu);
+	if (pincpu_do_pin(cpu) < 0) {
+		printf("Error pinning thread %d to cpu %d\n", arg->id, cpu);
+	}
 }
 
 int main(int argc, char **argv) {
@@ -105,6 +138,7 @@ int main(int argc, char **argv) {
 	struct experiment *exp = calloc(1, sizeof(struct experiment));
 	struct timespec ts;
 	struct timeval tv;
+	struct spin spin;
 	parse_opts(argc, argv, exp);
 
 	nr_iter = (exp->duration / USEC_PER_SEC) * exp->freq;
@@ -112,7 +146,11 @@ int main(int argc, char **argv) {
 	/*
 	ts.tv_nsec = (NSEC_PER_SEC / exp->freq) - exp->spin * 1000;
 	*/
-	spin_init(exp->nr_thread);
+	spin.n = exp->nr_thread;
+	spin.data = exp;
+	spin.init = pulse_pincpu;
+	spin.done = NULL;
+	spin_init(&spin);
 	for (i = 0; i < nr_iter; i++) {
 		// sync
 		gettimeofday(&tv, NULL);
@@ -123,7 +161,7 @@ int main(int argc, char **argv) {
 		ts.tv_nsec = sync * 1000;
 		nanosleep(&ts, NULL);
 		printf("before_spin=%d\n", i);
-		spin(exp->spin);
+		spin_run(exp->spin);
 		printf("after_spin=%d\n", i);
 	}
 	spin_exit();
