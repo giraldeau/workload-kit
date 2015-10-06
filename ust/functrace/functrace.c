350d7e5411d3e04e9fca3d52b8bc503db204d06d
Francis Giraldeau <francis.giraldeau@gmail.com>
2015-10-06 12:24:24 -0400

    Avoid inline function in functrace
    It seems that the compiler decided to inline the functions even with
    __attribute__((noinline)).
    
    Using function pointer is a trick that prevent the compiler to inline a
    function. The trace now correctly shows the function entry and exit.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- ust/functrace/functrace.c
+++ ust/functrace/functrace.c
@@ -6,31 +6,35 @@
 
 #include <stdio.h>
 
+int (*foo_ptr)(int);
+int (*bar_ptr)(int);
+int (*baz_ptr)(int);
+
+int baz(int n)
+{
+    n *= 3;
+    n /= 4;
+    return n;
+}
+
+int bar(int n)
+{
+    int i = 0;
+    while(i < n)
+    {
+        i = i << 1;
+        baz_ptr(i++);
+    }
+    return i;
+}
+
 int foo(int n)
 {
 	int i, x;
 
 	for (i=0; i<n; i++)
-		x = bar(i);
+		x = bar_ptr(i);
 	return x;
-}
-
-int bar(int n)
-{
-	int i = 0;
-	while(i < n)
-	{
-		i = i << 1; 
-		baz(i++);
-	}
-	return i;
-}
-
-int baz(int n)
-{
-	n *= 3;
-	n /= 4;
-	return n; 
 }
 
 int main(int argc, char **argv)
@@ -38,7 +42,11 @@ int main(int argc, char **argv)
 	if (argc < 2)
 		return -1;
 
+	foo_ptr = foo;
+	bar_ptr = bar;
+	baz_ptr = baz;
+
 	int n = atoi(argv[1]);
-	printf("result %d\n", foo(n));
+	printf("result %d\n", foo_ptr(n));
 	return 0; 
 }
